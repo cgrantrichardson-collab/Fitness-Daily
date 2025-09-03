26f1202e6420f2b9db832c350b9a246177c71a2c
cgrantrichardson-collab <cgrantrichardson@gmail.com>
2025-09-03 06:46:05 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-// service worker placeholder
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open('fd-v12').then(c=>c.addAll([
+    './','index.html','manifest.webmanifest',
+    'icon-512.png','icon-192.png','icon-180.png','favicon.png','splash-portrait.png'
+  ])));
+});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
