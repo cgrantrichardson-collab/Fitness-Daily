2977a8bfb3d75c35259a99f9d19ed299dccefb0d
cgrantrichardson-collab <cgrantrichardson@gmail.com>
2025-09-03 08:19:32 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('fd-v12').then(c=>c.addAll([
+  e.waitUntil(caches.open('fd-v15').then(c=>c.addAll([
     './','index.html','manifest.webmanifest',
     'icon-512.png','icon-192.png','icon-180.png','favicon.png','splash-portrait.png'
   ])));
