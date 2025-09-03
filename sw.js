43bef38fc2c74af0112b087d9b2b197805371420
cgrantrichardson-collab <cgrantrichardson@gmail.com>
2025-09-03 12:55:38 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open('v1').then(function(cache) {
-    return cache.addAll([
-      '/',
-      '/index.html'
-    ]);
-  }));
-});
-self.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then(function(response) {
-    return response || fetch(e.request);
-  }));
-});
+
+const CACHE='fdaily-v18-1';
+const ASSETS=['./','index.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png','favicon.png','splash-portrait.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
