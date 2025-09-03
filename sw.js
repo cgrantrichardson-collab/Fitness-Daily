a80a1807fd58c4aff1a4da3f1ac7c85c2e19b31d
cgrantrichardson-collab <cgrantrichardson@gmail.com>
2025-09-03 12:28:49 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('fdaily-v19').then(c=>c.addAll(['./','index.html','manifest.webmanifest'])))})
+const CACHE='fdaily-v20';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest','favicon.png','icon-180.png','icon-192.png','icon-512.png','splash-portrait.png'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
